1a2c948044a5c0d9ad87f50a04c5e881a9f7b3e3
Art Ejupi <artej02@gmail.com>
2023-08-27 18:58:38 +0200

    Starting API

---
+++ GamingNews/Controllers/APIController.cs
@@ -0,0 +1,24 @@
+﻿using GamingWeb.Custom.DatabaseHelpers;
+using GamingWeb.Custom.Models.Footer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GamingNews.Controllers
+{
+    [AllowAnonymous]
+    public class APIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetFooter()
+        {
+            var footer = (await new Query().SelectSingle<Footer>("Select * from Footer")).Result;
+
+            return Ok(footer);
+        }
+    }
+}
