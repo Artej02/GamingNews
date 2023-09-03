102c5bbaaa4b8a24ccc6fda8ac513d83cb70cf1f
AnnamoreaTiaraNixha <75945587+AnnamoreaTiaraNixha@users.noreply.github.com>
2023-09-03 04:32:11 -0700

    Update HomeController.cs

--- GamingNews/Controllers/HomeController.cs
+++ GamingNews/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using GamingWeb.Custom.DatabaseHelpers;
 using GamingWeb.Models;
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using GamingWeb.Custom.Helpers;
+using GamingWeb.Custom.Models.User;
 
 namespace GamingWeb.Controllers
 {
@@ -29,8 +30,32 @@ namespace GamingWeb.Controllers
             int thisYear = DateTime.Now.Year, thisMonth = DateTime.Now.Month;
             int lastDayOfThisMonth = DateTime.DaysInMonth(thisYear, thisMonth);
 
+            int userID = new AuthorizeHelper(HttpContext).GetUserID();
+
             ViewBag.ReceiptsCountThisMonth = (await new Query().SelectSingle<int>($"Select count(*) from Receipts")).Result;
             //var response1 = new CRUDProcedureGenerator().GenerateProcedure("Qualification");
+
+            ViewBag.NewsCount = (await new Query().SelectSingle<int>("SELECT COUNT(*) FROM News WHERE CreatedUserId = @UserID AND MONTH(ReleaseDate) = @Month AND YEAR(ReleaseDate) = @Year", new
+            {
+                @UserID = userID,
+                @Month = DateTime.Now.Month,
+                @Year = DateTime.Now.Year
+            })).Result;
+
+            ViewBag.AnnouncementsCount = (await new Query().SelectSingle<int>("SELECT COUNT(*) FROM Announcement WHERE CreatedUserId = @UserID AND MONTH(DisplayDate) = @Month AND YEAR(DisplayDate) = @Year", new
+            {
+                @UserID = userID,
+                @Month = DateTime.Now.Month,
+                @Year = DateTime.Now.Year
+            })).Result;
+
+            ViewBag.DocumentsCount = (await new Query().SelectSingle<int>("SELECT COUNT(*) FROM Documents WHERE CreatedUserId = @UserID AND MONTH(DisplayDate) = @Month AND YEAR(DisplayDate) = @Year", new
+            {
+                @UserID = userID,
+                @Month = DateTime.Now.Month,
+                @Year = DateTime.Now.Year
+            })).Result;
+
             return View();
         }
 
